ad10dfed02598be0286190bd2f5c2bb2b5c81e50
Ira <ira.d.sanchez@gmail.com>
2020-02-15 10:06:43 -0700

    Add fixes to header responsiveness
    - Added flex wrap to the nav
    - Added some padding to the link with my name, so the border wasn't huge

--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -5,43 +5,43 @@ import styled from "styled-components"
 
 const StyledNav = styled.nav`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-evenly;
   align-items: center;
   padding: 20px 0;
   box-shadow: 0px 3px 10px 1px rgba(222, 184, 135, 0.7); /*color name is BurlyWood, using rgba for adjusted opacity */
   & ${StyledNav}:nth-child(1) {
+    height: 4rem;
+    padding: 2rem 5%;
+    margin: 0 40% 1rem;
     border: double 8px #41403e;
     border-radius: 255px 15px 225px 15px/49px 125px 35px 59px;
-    padding: 5px 2%;
   }
   & ${StyledNav}:nth-child(2) {
-    border: solid 2px #41403e;
     border-radius: 255px 72px 132px 27px/105px 220px 57px 100px;
-    padding: 5px 2%;
   }
   & ${StyledNav}:nth-child(3) {
-    border: solid 2px #41403e;
     border-radius: 275px 55px 405px 145px/55px 245px 150px 255px;
-    padding: 5px 2%;
   }
   & ${StyledNav}:nth-child(4) {
-    border: solid 2px #41403e;
     border-radius: 305px 95px 225px 65px/86px 325px 85px 100px;
-    padding: 5px 2%;
   }
   & ${StyledNav}:nth-child(5) {
-    border: solid 2px #41403e;
     border-radius: 20px 105px 225px 15px/100px 51px 35px 52px;
-    padding: 5px 2%;
   }
 `
 
 const StyledLink = styled(Link)`
   display: flex;
   justify-content: center;
-  width: fit-content;
+  align-items: center;
+  width: 22%;
+  height: 3rem;
   color: black;
   text-decoration: none;
+  border: solid 2px #41403e;
+  /* padding: 10px 3%;
+  margin: 0 2%; */
 `
 
 const Header = ({ siteTitle }) => (
