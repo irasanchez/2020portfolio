bb81104e060f11f90f9193fdb54b02ad287849d0
Ira <ira.d.sanchez@gmail.com>
2020-06-17 10:25:30 -0700

    Debugging gatsby build

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -1,20 +1,20 @@
-{
-  /* GitHub Icon: <div>Icon made from <a href="http://www.onlinewebfonts.com/icon">Icon Fonts</a> is licensed by CC BY 3.0</div> 
-  
-  CSS, JS, LinkedIn Icon: <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-  
-
-
-  HTML Icon: https://www.svgrepo.com/svg/17484/html-5-logo
-
-
-  */
-}
-
 import React from "react"
 
 const ThankYou = () => {
-  return <div></div>
+  return (
+    <div>
+      {/* GitHub Icon: <div>Icon made from <a href="http://www.onlinewebfonts.com/icon">Icon Fonts</a> is licensed by CC BY 3.0</div> 
+    
+    CSS, JS, LinkedIn Icon: <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    
+  
+  
+    HTML Icon: https://www.svgrepo.com/svg/17484/html-5-logo
+  
+  
+    */}
+    </div>
+  )
 }
 
 export default ThankYou
