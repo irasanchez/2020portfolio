58d007baaad61869f4498db285ae7a87936da363
Ira <ira.d.sanchez@gmail.com>
2020-06-17 10:45:04 -0700

    More debugging

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import Nav from "./nav"
+import Nav from "../components/Nav"
 
 import "typeface-lateef"
 import "typeface-patrick-hand-sc"
