274e532094c1d92488cbed55eba76382d27a2911
Ira <ira.d.sanchez@gmail.com>
2020-06-22 18:10:39 -0700

    Add skill responsiveness

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,52 +10,99 @@ import styled from "styled-components"
 import randomCornerMaker from "../helpers/randomCornerMaker"
 
 const Section = styled.section`
-  display: flex;
   margin-top: 70px;
-  flex-wrap: wrap;
 
-  h3 {
-    margin: auto;
-    padding: 0;
-    width: 25%;
-    text-align: left;
-  }
-  .skills {
-    width: 90%;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    padding-left: 2rem;
-  }
   sub {
     width: 100%;
     text-align: right;
   }
+
+  @media (max-width: 900px) {
+    /* puts the title over the skills */
+    flex-direction: column;
+  }
 `
+
+const EverythingButSub = styled.div`
+  /* makes more sense when you see jsx, but the items need to be grouped together so that the sub is on bottom. */
+  display: flex;
+  flex-wrap: wrap;
+  @media (max-width: 900px) {
+    /* stacks the individual skills */
+    width: 80%;
+    margin: 0 auto;
+  }
+`
+
+const H3 = styled.h3`
+  margin: auto;
+  padding: 0;
+  width: 25%;
+  text-align: left;
+  min-width: 200px;
+
+  display: flex;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    margin: 0;
+  }
+`
+
+const SkillsContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  width: 90%;
+  padding-left: 2rem;
+
+  @media (max-width: 900px) {
+    margin: 0;
+    padding: 0;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+`
+
 const Skill = styled.div`
   display: flex;
+  flex-direction: row;
   justify-content: space-around;
   align-items: center;
+  flex-wrap: nowrap;
+  flex-grow: 1;
   width: 27%;
+  min-width: 190px;
   margin: 0.5rem 0;
 
-  /* background-color: teal;
-  color: cornsilk; */
   border: 2px solid teal;
 
   p {
     padding: 0.25rem;
     margin: 0%;
     min-width: fit-content;
-    width: 70%;
+    width: 65%;
     text-align: center;
+  }
+  @media (max-width: 900px) {
+    margin: 10px 1%;
+    width: 45%;
+    min-width: 190px;
   }
 `
 const Icon = styled.img`
-  width: 30%;
+  width: 3.5rem;
   padding: 0.75rem;
   padding-left: 1rem;
   margin: 0;
+
+  @media (max-width: 900px) {
+    padding: 0.25rem;
+    width: 20%;
+  }
 `
 
 const skillData = [
@@ -70,9 +117,12 @@ const skillData = [
 const Skills = () => {
   return (
     <Section>
-      <div>
-        <h3>I have experience with these technologies 👉</h3>
-        <div className="skills">
+      <EverythingButSub>
+        <H3>
+          I have experience with these technologies
+          <span aria-role="img"> 🔨</span>
+        </H3>
+        <SkillsContainer>
           {skillData.map((skill) => {
             return (
               <Skill
@@ -87,8 +137,8 @@ const Skills = () => {
               </Skill>
             )
           })}
-        </div>
-      </div>
+        </SkillsContainer>
+      </EverythingButSub>
       <sub>*LESS and SASS included</sub>
     </Section>
   )
